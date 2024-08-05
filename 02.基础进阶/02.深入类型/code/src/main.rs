1d3f0266c8dd965085024eb0a1817c607e5c6785
chenqin <490358423@qq.com>
2024-08-05 17:51:46 +0800

    类型转换

---
+++ 02.基础进阶/02.深入类型/code/src/main.rs
@@ -0,0 +1,13 @@
+use std::sync::Arc;
+
+#[derive(Clone)]
+struct Container<T>(Arc<T>);
+
+fn clone_containers<T>(foo: &Container<i32>, bar: &Container<T>) {
+    let foo_cloned = foo.clone();
+    let bar_cloned = bar.clone();
+}
+
+fn main(){
+    
+}
